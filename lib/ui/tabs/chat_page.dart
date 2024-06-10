e7994f7241ce354d4c4661d94b85d50e7cbbe399
kunledarams2 <daramolaadekunle15@gmail.com>
2024-06-10 16:51:56 +0100

    second init

--- lib/ui/tabs/chat_page.dart
+++ lib/ui/tabs/chat_page.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+import '../../theme/graphikRegular.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: Column(),),);
+    return Scaffold(body: SafeArea(child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Text("Message Page",
+              textAlign: TextAlign.center,
+              style: graphikRegular(
+                  fontColor: Color(0xFF232220),
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600)).animate(effects: [
+      const SlideEffect(
+            duration: Duration(milliseconds: 900),
+            curve: Curves.easeIn,
+            begin: Offset(0, -1),
+            end: Offset(0, 0)),
+    ]),
+        )
+      ],
+    ),),);
   }
 }
